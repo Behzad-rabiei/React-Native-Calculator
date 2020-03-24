2149d93c5637acdff2245729561232f8afed7d77
Behzad-Rabiei <behzad.rabiei.77@gmail.com>
2020-03-24 12:00:39 +0430

    Home Screen Created

---
+++ App/screens/Home.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { Container } from "../component/Container";
+
+class Home extends Component {
+  render() {
+    return <Container />;
+  }
+}
+
+export default Home;

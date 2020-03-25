6582f3707130efdfe83b1ebcf20da4fb1665695f
Behzad-Rabiei <behzad.rabiei.77@gmail.com>
2020-03-25 22:35:00 +0430

    Styling Button Component

--- App/component/Button/styles.js
+++ App/component/Button/styles.js
@@ -1,8 +1,17 @@
+import { Dimensions } from "react-native";
 import EStyleSheet from "react-native-extended-stylesheet";
+
+const screen = Dimensions.get("window");
 
 export default EStyleSheet.create({
   button: {
-    backgroundColor: "$lightGray"
+    flex: 1,
+    backgroundColor: "$lightGray",
+    alignItems: "center",
+    justifyContent: "center",
+    height: screen.width / 4 - 10,
+    borderRadius: screen.width / 4,
+    margin: 5
   },
   text: {
     fontSize: 20,

9e568c5de712fdbea89980dac46e81b3d6fff290
Behzad-Rabiei <behzad.rabiei.77@gmail.com>
2020-03-26 02:14:10 +0430

    Reducer Updated

--- App/reducers/index.js
+++ App/reducers/index.js
@@ -1,5 +1,19 @@
-const initialState = {};
+import { SET_CURRENT_VALUE } from "../actions/actionType";
 
-const rootReducer = (state = initialState, action) => {};
+const initialState = {
+  currentValue: "0"
+};
+
+const rootReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case SET_CURRENT_VALUE: {
+      return {
+        currentValue: action.value
+      };
+    }
+    default:
+      return state;
+  }
+};
 
 export default rootReducer;

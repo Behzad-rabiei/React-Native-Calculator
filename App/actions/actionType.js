2108b2e36fb52b86de7b15dc5df91229a130ea2e
Behzad-Rabiei <behzad.rabiei.77@gmail.com>
2020-03-27 01:03:32 +0430

    Action:clearValue Created

--- App/actions/actionType.js
+++ App/actions/actionType.js
@@ -1,3 +1,4 @@
 export const SET_CURRENT_VALUE = "SET_CURRENT_VALUE";
 export const SET_PREVIOUS_VALUE = "SET_PREVIOUS_VALUE";
+export const CLEAR_VALUE = "CLEAR_VALUE";
 export const SET_OPERATOR = "SET_OPERATOR";
--- App/actions/index.js
+++ App/actions/index.js
@@ -1,7 +1,8 @@
 import {
   SET_CURRENT_VALUE,
   SET_PREVIOUS_VALUE,
-  SET_OPERATOR
+  SET_OPERATOR,
+  CLEAR_VALUE
 } from "./actionType";
 
 export const setCurrentValue = number => {
@@ -22,5 +23,11 @@ export const setOperator = operator => {
   return {
     type: SET_OPERATOR,
     value: operator
+  };
+};
+
+export const clearValue = () => {
+  return {
+    type: CLEAR_VALUE
   };
 };

9b7ce56e5bbe0e31c0eab389e6f711e144d9b2d9
Behzad-Rabiei <behzad.rabiei.77@gmail.com>
2020-03-26 23:31:21 +0430

    Action: setPreviousValue Created

--- App/actions/index.js
+++ App/actions/index.js
@@ -1,8 +1,15 @@
-import { SET_CURRENT_VALUE } from "./actionType";
+import { SET_CURRENT_VALUE, SET_PREVIOUS_VALUE } from "./actionType";
 
 export const setCurrentValue = number => {
   return {
     type: SET_CURRENT_VALUE,
+    value: number
+  };
+};
+
+export const setPreviousValue = number => {
+  return {
+    type: SET_PREVIOUS_VALUE,
     value: number
   };
 };
